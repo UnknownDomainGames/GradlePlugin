772b7668ac7da0a6737d07e2131dab329ec69ebb
xtex <zbwxtex@qq.com>
2021-10-22 18:40:29 +0800

    style(runClient): optimize style

--- src/main/java/engine/mod/gradle/task/RunClientTask.java
+++ src/main/java/engine/mod/gradle/task/RunClientTask.java
@@ -10,40 +10,36 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class RunClientTask extends JavaExec {
 
     public RunClientTask() {
         super();
         getMainClass().set("engine.client.launch.Bootstrap");
-        setArgs(List.of());
-        setJvmArgs(List.of("--add-opens", "java.base/java.lang=ALL-UNNAMED","-agentlib:jdwp=transport=dt_socket,server=n,address=xtex:1075,suspend=y"));
-        dependsOn(getProject().getTasks().getByName(JavaPlugin.JAR_TASK_NAME));
+        args(List.of());
+        jvmArgs(List.of("--add-opens", "java.base/java.lang=ALL-UNNAMED"));
+        setClasspath(new FileCollectionAdapter(new MinimalFileSet() {
+            @Override
+            @Nonnull
+            public Set<File> getFiles() {
+                List<File> files = new ArrayList<>(getProject().getConfigurations().getByName(JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME).getFiles());
+                files.addAll(getProject().getTasks().getByName(JavaPlugin.COMPILE_JAVA_TASK_NAME).getOutputs().getFiles().getFiles());
+                files.addAll(getProject().getTasks().getByName(JavaPlugin.PROCESS_RESOURCES_TASK_NAME).getOutputs().getFiles().getFiles());
+                return Set.copyOf(files);
+            }
+
+            @Override
+            @Nonnull
+            public String getDisplayName() {
+                return "Engine Runtime Classpath";
+            }
+        }));
+        dependsOn(getProject().getTasks().getByName(JavaPlugin.COMPILE_JAVA_TASK_NAME));
+        dependsOn(getProject().getTasks().getByName(JavaPlugin.PROCESS_RESOURCES_TASK_NAME));
     }
 
     @Override
     public void exec() {
-        { // Configure classpath
-            setClasspath(new FileCollectionAdapter(new MinimalFileSet() {
-                @Override
-                public @Nonnull
-                Set<File> getFiles() {
-                    List<File> files = new ArrayList<>(getProject().getConfigurations().getByName(JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME).getFiles());
-                    files.addAll(getProject().getTasks().getByName(JavaPlugin.COMPILE_JAVA_TASK_NAME).getOutputs().getFiles().getFiles());
-                    files.addAll(getProject().getTasks().getByName(JavaPlugin.PROCESS_RESOURCES_TASK_NAME).getOutputs().getFiles().getFiles());
-                    System.out.println(files);
-                    return Set.copyOf(files);
-                }
-
-                @Override
-                public @Nonnull
-                String getDisplayName() {
-                    return "Engine Runtime Classpath";
-                }
-            }));
-        }
-
         super.exec();
     }
 }
